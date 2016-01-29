cfe5c13997d7403d653a452421741013362623ce
Gregor Santner <gdev@live.de>
2016-01-29 17:13:48 +0100

    v1.11: Marshmallow support, less ressources, fixes #1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 =============================
 
 ## General
-Latest modification: 17. Nov 2015 (v1.1)  
+Latest modification: 25. Jan 2016 (v1.11)  
 License: **GPLv3**  
 XPosed module created and maintained by  
 Gregor Santner (<http://de-live-gdev.github.io>), 2014-  
@@ -10,25 +10,30 @@ Gregor Santner (<http://de-live-gdev.github.io>), 2014-
 ## Description
 This module adds a button to every app in App Info page.   
 When the XPrivacy button gets clicked, XPrivacy will be started in configuration mode for the app.  
-Personally I tried the module on AOSP based ROMs with version 4.2.2, 4.4, 5.0 & 5.1.  
+Personally I tried the module on AOSP&CM based ROMs with version 4.2.2, 4.4, 5.0, 5.1 & 6.0.  
 
 ## Downloads
 
 * [XPosed module repository](http://repo.xposed.info/module/de.live.gdev.xprivacyappinfo)  
 * [Release archive](https://github.com/de-live-gdev/XPrivacyAppInfo/releases)  
+* [F-Droid (author repo)](http://de-live-gdev.github.io/fdroid/xprivacy-in-app-info/)  
 
 ## Releases
 
 <table>
- <thead>
-	<td><b>Version</b></td>
+ <thead><td><b>Version</b></td>
 	<td><b>Date</b></td>
-  <td><b>Changelog</b></td>
+	<td><b>Changelog</b></td>
  </thead>
  <tr>
-    <td>v1.1</td>
-   <td>17. Nov 2015</td>
-   <td>Gradle build system, XPosed API v54</td>
+	<td>v1.11</td>
+	<td>29. Jan 2016</td>
+	<td>Marshmallow Support</td>
+ </tr>
+ <tr>
+	<td>v1.1</td>
+	<td>17. Nov 2015</td>
+	<td>Gradle build system, XPosed API v54</td>
  </tr>
  <tr>
 	<td>v1.0</td>
@@ -40,7 +45,7 @@ Personally I tried the module on AOSP based ROMs with version 4.2.2, 4.4, 5.0 & 
 ## Donations
 <table>
  <tr>
-	<td><a href="bitcoin:1B9ZyYdQoY9BxMe9dRUEKaZbJWsbQqfXU5?amount=0.05&label=de-live-gdev">
+	<td><a href="bitcoin:1B9ZyYdQoY9BxMe9dRUEKaZbJWsbQqfXU5?amount=0.01&label=de-live-gdev">
     <img src="http://de-live-gdev.github.io/assets/img/personal/bitcoin/bitcoin_gdev-live-gdev_150px.png"/></a></td>
 	<td>If you like my work on <b>XPrivacy Button in App Info</b> and Open-Source, or just want to spend me a coke, consider a small donation to my bitcoin address:
 
@@ -53,16 +58,17 @@ Personally I tried the module on AOSP based ROMs with version 4.2.2, 4.4, 5.0 & 
 
 Downloads from official XPosed module repository:
 
-|      Date     |  DL overall  |   DL (24h) |
-|---------------|--------------|------------|
-| 15. Nov 2015  |    33.075    |  84        |
+|      Date     |  Downloads   |
+|---------------|--------------|
+| 25. Jan 2016  |    42.668    |
+| 15. Nov 2015  |    33.075    |
 
 
 ## Screenshot
 
-v1.0                       |           v1.1
-:-------------------------:|:-------------------------:
-<img src="https://cloud.githubusercontent.com/assets/6735650/5047479/2b508316-6c16-11e4-9622-b5c225484615.png" height="50%" width="50%" >  |  <img src="https://cloud.githubusercontent.com/assets/6735650/11198591/4884e59a-8cc6-11e5-967b-5d220fb306a4.png" height="35%" width="35%" >
+v1.11                      |          v1.1            |           v1.0
+:-------------------------:|:------------------------:|:-------------------------:
+<img src="https://cloud.githubusercontent.com/assets/6735650/12679911/26d9efa6-c6a7-11e5-953d-43350754ad38.png" height="35%" width="35%" > | <img src="https://cloud.githubusercontent.com/assets/6735650/11198591/4884e59a-8cc6-11e5-967b-5d220fb306a4.png" height="35%" width="35%" > | <img src="https://cloud.githubusercontent.com/assets/6735650/5047479/2b508316-6c16-11e4-9622-b5c225484615.png" height="50%" width="50%" >   
 
 ## Sources
-Based on [https://github.com/qwerty12/PlayStoreLinkInAppInfo](https://github.com/qwerty12/PlayStoreLinkInAppInfo) by [qwerty12](https://github.com/qwerty12).  
+Initialy based on [https://github.com/qwerty12/PlayStoreLinkInAppInfo](https://github.com/qwerty12/PlayStoreLinkInAppInfo) by [qwerty12](https://github.com/qwerty12).  
--- app/src/main/java/de/live/gdev/xprivacyappinfo/XPrivacyInAppInfo.java
+++ app/src/main/java/de/live/gdev/xprivacyappinfo/XPrivacyInAppInfo.java
@@ -5,82 +5,110 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class XPrivacyInAppInfo implements IXposedHookLoadPackage {
-
-    private final static String PACKAGE_SETTINGS = "com.android.settings";
+public class XPrivacyInAppInfo implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
     private final static String PACKAGE_XPRIVACY = "biz.bokhorst.xprivacy";
+    private final static String PACKAGE_SETTINGS = "com.android.settings";
+
+    private final static String BUTTON_TEXT = "XPrivacy";
+    private Button myAppInfoButton = null;
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals(PACKAGE_SETTINGS)) {
             try {
                 final Class<?> classInstalledAppDetails = XposedHelpers.findClass(PACKAGE_SETTINGS + ".applications.InstalledAppDetails", lpparam.classLoader);
-
-                XposedHelpers.findAndHookMethod(classInstalledAppDetails, "onCreateView", LayoutInflater.class, ViewGroup.class, Bundle.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
-                        View mNotificationSwitch = (View) XposedHelpers.getObjectField(param.thisObject, "mNotificationSwitch");
-                        View mUninstallButton = (View) XposedHelpers.getObjectField(param.thisObject, "mUninstallButton");
-                        ViewGroup viewGroup = (ViewGroup) mNotificationSwitch.getParent();
-                        final Context context = viewGroup.getContext();
-
-                        Button xPrivacyButton = new Button(context);
-                        xPrivacyButton.setText("XPrivacy");
-                        xPrivacyButton.setVisibility(View.GONE);
-                        xPrivacyButton.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                final int packageUid = (Integer) v.getTag();
-                                if (packageUid != -1)
-                                {
-                            		Intent intent = new Intent("biz.bokhorst.xprivacy.action.APPLICATION");
-                            		intent.setComponent(new ComponentName(PACKAGE_XPRIVACY, "biz.bokhorst.xprivacy.ActivityApp"));
-                            		intent.putExtra("Uid", packageUid);
-                            		context.startActivity(intent);
-                                }
-                            }
-                        });
-                        viewGroup.addView(xPrivacyButton, viewGroup.indexOfChild(mNotificationSwitch) + 1, mUninstallButton.getLayoutParams());
-
-                        XposedHelpers.setAdditionalInstanceField(param.thisObject, "xPrivacyButton", xPrivacyButton);
-                    }
-                });
-
                 XposedHelpers.findAndHookMethod(classInstalledAppDetails, "refreshUi", new XC_MethodHook() {
-                    @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Button xPrivacyButton = (Button) XposedHelpers.getAdditionalInstanceField(param.thisObject, "xPrivacyButton");
-                        xPrivacyButton.setTag(-1);
-                        if ((!((Boolean) param.getResult())) || xPrivacyButton != null && xPrivacyButton.getVisibility() == View.VISIBLE)
+                        if (myAppInfoButton == null || myAppInfoButton.getVisibility() != View.INVISIBLE)
                             return;
 
-                 
                         final int packageUid = ((ApplicationInfo) XposedHelpers.getObjectField(XposedHelpers.getObjectField(param.thisObject, "mAppEntry"), "info")).uid;
-                        PackageManager pm = (PackageManager) XposedHelpers.getObjectField(param.thisObject, "mPm");
+                        final PackageManager pm = (PackageManager) XposedHelpers.getObjectField(param.thisObject, "mPm");
 
-                        // Throws exception if app isnt available -> no button visible
-                        try{
-							pm.getPackageInfo(PACKAGE_XPRIVACY, PackageManager.GET_ACTIVITIES);
-							
-							xPrivacyButton.setTag(packageUid);
-							xPrivacyButton.setVisibility(View.VISIBLE);
-                            }
-                        catch(NameNotFoundException ex1){}
+                        boolean isXPrivacyInstalled = isAppInstalled(pm, PACKAGE_XPRIVACY);
+                        myAppInfoButton.setTag(isXPrivacyInstalled ? packageUid : -1);
+                        myAppInfoButton.setVisibility(isXPrivacyInstalled ? View.VISIBLE : View.GONE);
                     }
                 });
-            } catch (Throwable t) { XposedBridge.log(t); }
+            } catch (Throwable t) {
+                XposedBridge.log(t);
+            }
+        }
+    }
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+        if (!resparam.packageName.equals(PACKAGE_SETTINGS))
+            return;
+
+        resparam.res.hookLayout(PACKAGE_SETTINGS, "layout", "installed_app_details", new XC_LayoutInflated() {
+            // App Detail Layout was inflated
+            public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+                try {
+                    ViewGroup linearLayoutAllDetails = (ViewGroup) liparam.view.findViewById(
+                            liparam.res.getIdentifier("all_details", "id", PACKAGE_SETTINGS));
+
+                    myAppInfoButton = newMyAppInfoButton(linearLayoutAllDetails.getContext());
+                    linearLayoutAllDetails.addView(myAppInfoButton, 1);
+
+                } catch (Exception ex) {
+                    XposedBridge.log(ex);
+                }
+            }
+        });
+    }
+
+    /**
+     * Generate a new App Info Button
+     *
+     * @param context Contexxt
+     * @return A button
+     */
+    public Button newMyAppInfoButton(final Context context) {
+        Button button = new Button(context);
+        button.setText(BUTTON_TEXT);
+        button.setVisibility(View.INVISIBLE);
+        button.setTag("-1");
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final int packageUid = (Integer) v.getTag();
+                if (packageUid != -1) {
+                    Intent intent = new Intent(PACKAGE_XPRIVACY + ".action.APPLICATION");
+                    intent.setComponent(new ComponentName(PACKAGE_XPRIVACY, PACKAGE_XPRIVACY + ".ActivityApp"));
+                    intent.putExtra("Uid", packageUid);
+                    context.startActivity(intent);
+                }
+            }
+        });
+        return button;
+    }
+
+    /**
+     * Check if application is installed
+     *
+     * @param pm  PackageManager
+     * @param uri packageName
+     * @return true if app is installed
+     */
+    private boolean isAppInstalled(final PackageManager pm, final String uri) {
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
         }
     }
 }
